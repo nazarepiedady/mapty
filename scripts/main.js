a436258aec350b8b8a207ba302988c54a0cba30b
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-02 12:40:28 +0100

    set coords, distance and duration into workout constructor

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,10 @@ class Workout {
   clicks = 0;
   date = new Date();
   id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
 }
