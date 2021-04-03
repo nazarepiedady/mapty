1a9a42b982ed50037bcf2f7ccfb9c6fb4202da0c
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-03 20:20:52 +0100

    create the load map method of app

--- scripts/main.js
+++ scripts/main.js
@@ -107,4 +107,25 @@ class App {
         }
       );
   }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    this.map = L.map('map').setView(coords, this.mapZoomLevel);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+
+    // Handling clicks on map
+    this.map.on('click', this._showForm.bind(this));
+
+    this.workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
+  }
 }
